cac1f8e5994778b5201a3c98b893108e34ca43d4
Alberto Scotto <scotto.alberto.86@gmail.com>
2022-03-09 12:18:18 +0100

    aggiungo riga di log nel test

--- src/test/java/me/alb_i986/retry/RetryOnExceptionTest.java
+++ src/test/java/me/alb_i986/retry/RetryOnExceptionTest.java
@@ -4,14 +4,16 @@ import me.alb_i986.retry.testutils.IntermediateException;
 import me.alb_i986.retry.testutils.SubException;
 import me.alb_i986.retry.testutils.SuperException;
 import org.junit.Test;
+import org.slf4j.Logger;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class RetryOnExceptionTest {
-
+private static final Logger LOGGER = LoggerFactory.getLogger(RetryOnExceptionTest.class);
     @Test
     public void whenOff() {
+LOGGER.debug("log log log");
         RetryOnException sut = new RetryOnException(null);
 
         assertFalse(sut.isOn());
